8d313d516bb47f9d48c9256029f5a2a03e78dc65
Stepan Polevshchikov <spolevshchikov@ozon.ru>
2020-11-09 23:25:28 +0300

    [FE-0003] feat: добавляет ant.design пример с рендером формы

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,49 @@
-import { VueComponent, Component } from '~/types'
+import { VueComponent, Component, Prop } from '~/types'
+import { Form, Input, Button } from 'ant-design-vue'
+// import { WrappedFormUtils } from 'ant-design-vue'
 
+import 'ant-design-vue/dist/antd.css';
 @Component
-export default class Index extends VueComponent {
+export class Index extends VueComponent {
+	@Prop() form!: Form['form']
+
+	// form = this.$form.createForm(this, {name: 'xxx'})
+
+	handleSubmit(e: Event) {
+			e.preventDefault()
+			this.form.validateFields((err, values) => {
+			  if (!err) {
+				console.log('Received values of form: ', values);
+			  }
+			});
+	}
+
 	render() {
 		return (
 			<div>
 				Hi from tsx...
 					
+				<Form
+					form={this.form}
+					onSubmit={this.handleSubmit}
+				>
+					<Form.Item>
+						<Input
+							placeholder='placeholder'
+							v-decorator={[
+								'login',
+								{rules: [{required: true, message: 'Логин обязателен'}]}
+							]}
+						/>
+					</Form.Item>
+					<Button
+						type='primary'
+						htmlType='submit'
+					>
+						Log In
+					</Button>
+				</Form>
+
 				<nuxt-link
 					to={{
 						name: 'live',
@@ -21,3 +58,9 @@ export default class Index extends VueComponent {
 		)
 	}
 } 
+
+export default Form.create({
+	name: 'testForm',
+	onFieldsChange: () => console.log('change'),
+	onValuesChange: () => console.log('cahnge 2')
+})(Index)
